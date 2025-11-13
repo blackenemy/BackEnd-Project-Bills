c1bb848fb07ab462e1100b9a217b0a1f14a95b1a
Blackenemy <enemyxiceblack@gmail.com>
2025-11-13 15:43:50 +0700

    refactor(bills): simplify UpdateBillDto by removing payment properties

--- src/bills/dto/update-bill.dto.ts
+++ src/bills/dto/update-bill.dto.ts
@@ -1,32 +1,4 @@
-import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
 import { CreateBillDto } from './create-bill.dto';
-import { Type } from 'class-transformer';
-import { IsOptional, ValidateNested } from 'class-validator';
-import { PaymentDto } from './payment.dto';
 
-export class UpdateBillDto extends PartialType(CreateBillDto) {
-  @ApiProperty({
-    type: PaymentDto,
-    description: 'ข้อมูลการชำระเงิน (สำหรับการอัปเดต)',
-    required: false,
-    example: {
-      customer_name: 'สมชาย ใจดี',
-      payment: 1500.00,
-      details: {
-        type: 'โอนเงิน',
-        date: new Date(),
-        bills: [
-          {
-            name: 'สินค้า B',
-            price: 200.00,
-            amount: 1
-          }
-        ]
-      }
-    }
-  })
-  @IsOptional()
-  @ValidateNested()
-  @Type(() => PaymentDto)
-  payment?: PaymentDto;
-}
+export class UpdateBillDto extends PartialType(CreateBillDto) {}
